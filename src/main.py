3f5a9230cc67d0fb9c2cf5a9262a5cfa10754513
Salvador Guzman <guzmansalv@gmail.com>
2018-04-19 18:50:54 -0700

    it works i guess

--- src/main.py
+++ src/main.py
@@ -2,95 +2,93 @@ import brotli
 import redis
 import bs4
 import requests
+import queue
+from multiprocessing.dummy import Pool as ThreadPool
+
+base = "http://23.95.221.108/page/"
+redisHash = 'ebooks'
+limit = 1268
+red = redis.StrictRedis()
+hashmap = red.hgetall(redisHash)
 
 
-class SGlobals:
-    base = "http://23.95.221.108/page/"
-    redisHash = 'ebooks'
-    limit = 1268
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 
-class SUtil:
-    @staticmethod
-    def remove_prefix(text, prefix):
-        if text.startswith(prefix):
-            return text[len(prefix):]
-        return text
+def remove_url(text):
+    return remove_prefix(text, 'https://it-eb.com/')
 
 
-    @staticmethod
-    def remove_url(text):
-        return SUtil.remove_prefix(text, 'https://it-eb.com/')
-
-
-    @staticmethod
-    def fnflatmap(func, collection):
-        new_list = []
-        for item in collection:
-            val = func(item)
-            if type(val) is list:
-                new_list = new_list + val
-            else:
-                new_list.append(val)
-
-        return new_list
-
-
-class SRedis:
-    red = redis.StrictRedis()
-    hashmap = red.hgetall(SGlobals.redisHash)
-
-
-    @staticmethod
-    def get_redis(url):
-        key = str.encode(url)
-        if SRedis.hashmap.get(key) is None:
-            print('Missed Http cache for url %s' % url)
-            html = requests.get(url).text
-            SRedis.red.hset(SGlobals.redisHash, url, brotli.compress(html.encode(), brotli.MODE_TEXT))
+def fnflatmap(func, collection):
+    new_list = []
+    for item in collection:
+        val = func(item)
+        if type(val) is list:
+            new_list = new_list + val
         else:
-            print('Hit Http cache for url %s' % url)
-            html = SRedis.hashmap.get(key)
-            html = brotli.decompress(html)
+            new_list.append(val)
 
-        return html
-
-    @staticmethod
-    def get_links(i):
-        url = SGlobals.base + str(i)
-
-        html = SRedis.get_redis(url)
-
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        arts = soup.findAll('article')
-
-        return [SUtil.remove_url(x.find('a').attrs['href']) for x in arts]
+    return new_list
 
 
+def get_redis(url, new_entries):
+    key = str.encode(url)
+    if hashmap.get(key) is None:
+        print('Missed Http cache for url %s' % url)
+        html = requests.get(url).text
 
-    @staticmethod
-    def links():
-        return SUtil.fnflatmap(SRedis.get_links, range(1, SGlobals.limit))
+        comp = brotli.compress(html.encode(), brotli.MODE_TEXT)
+        to_input = [key, comp]
+
+        new_entries.put(to_input)
+    else:
+        print('Hit Http cache for url %s' % url)
+        html = hashmap.get(key)
+        html = brotli.decompress(html)
+
+    return html
 
 
-    @staticmethod
-    def get_book(path):
-        url = SGlobals.base + path
+def get_links(i, new_entries):
+    url = base + str(i)
 
-        html = SRedis.get_redis(url)
+    html = get_redis(url, new_entries)
 
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        return soup
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    arts = soup.findAll('article')
 
-    @staticmethod
-    def books(ls):
-        return SUtil.fnflatmap(SRedis.get_book, ls)
+    return [remove_url(x.find('a').attrs['href']) for x in arts]
+
+
+def pmap(func, collection, cores=4):
+    pool = ThreadPool(cores)
+    new_entries = queue.LifoQueue()
+
+    results = pool.map(lambda x: func(x, new_entries), collection)
+    new_entries.join()
+    print('Got %d Http entries' % new_entries.qsize())
+    for i in range(new_entries.qsize()):
+        pair = new_entries.get()
+        key = pair[0]
+        val = pair[1]
+
+        print('Inserting Http entry %s with length %d' % (key, len(val)))
+        red.hset(redisHash, key, val)
+
+    return results
+
+
+def pflatmap(func, collection, cores=4):
+    results = pmap(func, collection, cores)
+    return [item for sublist in results for item in sublist]
 
 
 def main():
-    ls = SRedis.links()
-    b = SRedis.books(ls)
-    print(len(b))
+    ls = pflatmap(get_links, range(1, limit))
+    print(len(ls))
 
 
 main()
