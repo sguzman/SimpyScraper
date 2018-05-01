c8821abcd0bee6f0ca281d6c8e37619d9ffd3699
Salvador Guzman <guzmansalv@gmail.com>
2018-05-01 09:06:05 -0700

    fixed up stuff

--- src/main.py
+++ src/main.py
@@ -8,6 +8,12 @@ red = redis.StrictRedis()
 hashmap = red.hgetall('ebooks')
 
 
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+
 def get_redis(url):
     key = url.encode()
     if hashmap.get(key) is None:
@@ -34,7 +40,7 @@ def get_links(i):
     arts = soup.findAll('article')
     hrefs = [x.find('a').attrs['href'] for x in arts]
 
-    return [x.lstrip('https://it-eb.com/') for x in hrefs]
+    return [remove_prefix(x, 'https://it-eb.com/') for x in hrefs]
 
 
 def get_book(path):
@@ -48,7 +54,7 @@ def get_book(path):
     details = soup.find('div', class_='book-details').ul
     detail_keys = [x.get_text() for x in details.findAll('span')]
     detail_raw_vals = [x.get_text() for x in details.findAll('li')]
-    detail_dict = {k.rstrip(':').lower(): v.lstrip(k) for (k, v) in zip(detail_keys, detail_raw_vals)}
+    detail_dict = {k.rstrip(':').lower(): remove_prefix(v, k) for (k, v) in zip(detail_keys, detail_raw_vals)}
 
     return [arts,
             soup.find('input', attrs={'type': 'hidden', 'name': 'comment_post_ID'})['value'],
@@ -63,24 +69,23 @@ def get_host(i):
     return get_redis(url)
 
 
-def get_rapid_host(path):
-    url = f"http://23.95.221.108/{path}"
+def get_rapid_host(url):
     html = get_redis(url)
     soup = bs4.BeautifulSoup(html, 'html.parser')
 
     details = soup.find('div', class_='file-info').ul
     detail_keys = [x.get_text() for x in details.findAll('span')]
     detail_raw_vals = [x.get_text() for x in details.findAll('li')]
-    detail_dict = {k.rstrip(':').lower(): v.lstrip(k) for (k, v) in zip(detail_keys, detail_raw_vals)}
+    detail_dict = {k.rstrip(':').lower(): remove_prefix(v, k) for (k, v) in zip(detail_keys, detail_raw_vals)}
 
     return detail_dict,
 
 
 def main():
-    ls = [links for links in map(get_links, range(1, limit))]
-    bs = map(get_host, ls)
-    hs = map(get_host, bs)
-    xs = map(get_rapid_host, hs)
+    ls = [x for links in range(1, limit) for x in get_links(links)]
+    bs = [get_book(x) for x in ls]
+    hs = [get_host(x) for x in bs]
+    xs = [get_rapid_host(x) for x in hs]
     for i in xs:
         print(i)
 
