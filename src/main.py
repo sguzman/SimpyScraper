c2b570c680ae585f7fce8681f3eeb4394e42c94a
Salvador Guzman <guzmansalv@gmail.com>
2018-05-02 13:39:37 -0700

    focus on core details for now

--- src/main.py
+++ src/main.py
@@ -57,40 +57,27 @@ def get_book(path):
     detail_dict = {k.rstrip(':').lower(): remove_prefix(v, k) for (k, v) in zip(detail_keys, detail_raw_vals)}
 
     return [arts,
-            soup.find('input', attrs={'type': 'hidden', 'name': 'comment_post_ID'})['value'],
             soup.find('div', class_='book-cover').img['src'],
             detail_dict,
             soup.find('div', class_='entry-inner').get_text()
             ]
 
 
-def get_host(i):
-    url = f'http://23.95.221.108/download.php?id={i[1]}'
-    return get_redis(url)
-
-
-def get_rapid_host(url):
-    html = get_redis(url)
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-
-    details = soup.find('div', class_='file-info').ul
-    if details is None:
-        return {}
-
-    detail_keys = [x.get_text() for x in details.findAll('span')]
-    detail_raw_vals = [x.get_text() for x in details.findAll('li')]
-    detail_dict = {k.rstrip(':').lower(): remove_prefix(v, k) for (k, v) in zip(detail_keys, detail_raw_vals)}
-
-    return detail_dict,
-
-
 def main():
     ls = [x for links in range(1, limit) for x in get_links(links)]
     bs = [get_book(x) for x in ls]
-    hs = [get_host(x) for x in bs]
-    xs = [get_rapid_host(x) for x in hs]
-    for i in xs:
-        print(i)
+    for x in bs:
+        print(f"""
+        INSERT INTO ebooks (id, title, img, desc) VALUES (NULL, '{x[0]}', '{x[1]}', '{x[-1]}')
+        """)
+    print("""
+    CREATE TABLE ebooks(
+      id INTEGER PRIMARY KEY ASC AUTOINCREMENT NOT NULL UNIQUE,
+      title VARCHAR(255) UNIQUE,
+      img VARCHAR(200),
+      desc VARCHAR(2000)
+    );
+    """)
 
 
 main()
