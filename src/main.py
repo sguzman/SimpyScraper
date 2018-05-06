4eedae907de0fea526719039898af2168d7262fd
Salvador Guzman <guzmansalv@gmail.com>
2018-05-05 17:30:32 -0700

    working relational tables

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import bs4
 import requests
 import sqlite3
 
-limit = 1268
+limit = 1281
 red = redis.StrictRedis()
 hashmap = red.hgetall('ebooks')
 
@@ -51,6 +51,10 @@ def get_book(path):
 
     soup = bs4.BeautifulSoup(html, 'html.parser')
     arts = soup.find('h1', class_='post-title').get_text()
+    e_id = soup.find('input', {'type': 'hidden', 'name': 'comment_post_ID'})['value']
+    host = get_redis(f'http://23.95.221.108/download.php?id={e_id}')
+
+    categories = [x.get_text() for x in soup.find('p', class_='post-btm-cats').findAll('a')]
 
     details = soup.find('div', class_='book-details').ul
     detail_keys = [x.get_text() for x in details.findAll('span')]
@@ -60,7 +64,9 @@ def get_book(path):
     return [arts,
             soup.find('div', class_='book-cover').img['src'],
             detail_dict,
-            soup.find('div', class_='entry-inner').get_text()
+            soup.find('div', class_='entry-inner').get_text(),
+            host,
+            categories
             ]
 
 
@@ -69,10 +75,17 @@ def main():
     conn = sqlite3.connect("item.db")
     c = conn.cursor()
     c.execute("""
+            CREATE TABLE categories(
+              id INTEGER PRIMARY KEY ASC AUTOINCREMENT UNIQUE NOT NULL,
+              name VARCHAR(20) UNIQUE NOT NULL
+            );
+            """)
+
+    c.execute("""
         CREATE TABLE ebooks(
-          id INTEGER PRIMARY KEY ASC AUTOINCREMENT NOT NULL UNIQUE,
-          title VARCHAR(255) UNIQUE,
-          img VARCHAR(200) UNIQUE,
+          id INTEGER PRIMARY KEY ASC AUTOINCREMENT UNIQUE NOT NULL,
+          title VARCHAR(255) NOT NULL,
+          img VARCHAR(200),
           des VARCHAR(2000),
           isbn_10 VARCHAR(15),
           isbn_13 VARCHAR(20),
@@ -81,17 +94,30 @@ def main():
           pub_date DATE,
           size VARCHAR(15),
           pub VARCHAR(30),
-          pages VARCHAR(30)
+          pages VARCHAR(30),
+          host VARCHAR(30)
         );
         """)
+
+    c.execute("""
+        CREATE TABLE categories_data(
+          id INTEGER PRIMARY KEY ASC AUTOINCREMENT UNIQUE NOT NULL,
+          ebook_id INTEGER NOT NULL,
+          cat_id INTEGER NOT NULL,
+          FOREIGN KEY(ebook_id) REFERENCES ebooks(id),
+          FOREIGN KEY(cat_id) REFERENCES categories(id)
+        );
+        """)
+
     conn.commit()
     for i in range(1, limit):
         for path in get_links(i):
             book = get_book(path)
             try:
-                c.execute(
-                    "INSERT INTO ebooks (title, img, des, isbn_10, isbn_13, format, author, pub_date, size, pub, pages) "
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", [
+                out = c.execute(
+                    "INSERT INTO ebooks "
+                    "(title, img, des, isbn_10, isbn_13, format, author, pub_date, size, pub, pages, host) "
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", [
                         book[0],
                         book[1],
                         book[3],
@@ -103,12 +129,33 @@ def main():
                         book[2].get('size'),
                         book[2].get('publisher'),
                         book[2].get('pages'),
+                        book[4]
                     ])
-            except sqlite3.IntegrityError:
-                print(f'Skipping non-unique title, {book[0]}')
-                continue
+                conn.commit()
+                print(f'Inserted ({book[0]}, {book[1]}, ...) -> ebooks')
 
-        conn.commit()
+                ebook_id = out.lastrowid
+                for cat in book[5]:
+                    try:
+                        c.execute("""INSERT INTO categories (name) VALUES (?)""", [cat])
+                        print(f'Inserted ({cat}) -> categories')
+                    except sqlite3.IntegrityError:
+                        print(f'Skipping category {cat}')
+
+                    c.execute("""SELECT id FROM categories WHERE name = ?""", [cat])
+                    cat_id = c.fetchone()[0]
+                    try:
+                        c.execute("""INSERT INTO categories_data (ebook_id, cat_id) VALUES (?, ?)""",
+                                  [ebook_id, cat_id])
+                    except:
+                        print('Failed to insert into categories_data table')
+                        exit(0)
+                    print(f'Inserted [{book[0]} {cat}] ({ebook_id}, {cat_id}) -> categories_data')
+
+                conn.commit()
+            except sqlite3.IntegrityError as err:
+                print(err)
+                continue
 
     c.close()
     conn.close()
