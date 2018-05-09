c5203fd08579aa3b08061a82d0744c4977eac1fa
Salvador Guzman <guzmansalv@gmail.com>
2018-05-08 19:08:58 -0700

    added protobuf

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import brotli
-import redis
 import bs4
+import redis
 import requests
-import sqlite3
+from .items_pb2 import *
 
 limit = 1281
 red = redis.StrictRedis()
@@ -70,79 +70,13 @@ def get_book(path):
 
 # {'isbn-13', 'authors', 'format', 'publication date', 'size', 'publisher', 'isbn-10', 'pages'}
 def main():
-    conn = sqlite3.connect("item.db")
-    c = conn.cursor()
-    c.execute("""
-            CREATE TABLE categories(
-              id INTEGER PRIMARY KEY ASC AUTOINCREMENT UNIQUE NOT NULL,
-              name VARCHAR(20) UNIQUE NOT NULL
-            );
-            """)
-
-    c.execute("""
-        CREATE TABLE ebooks(
-          id INTEGER PRIMARY KEY ASC AUTOINCREMENT UNIQUE NOT NULL,
-          title VARCHAR(255) NOT NULL,
-          img VARCHAR(200),
-          des VARCHAR(2000),
-          isbn_10 VARCHAR(15),
-          isbn_13 VARCHAR(20),
-          format VARCHAR(10),
-          author VARCHAR(30),
-          pub_date DATE,
-          size VARCHAR(15),
-          pub VARCHAR(30),
-          pages VARCHAR(30),
-          host VARCHAR(30)
-        );
-        """)
-
-    c.execute("""
-        CREATE TABLE categories_data(
-          id INTEGER PRIMARY KEY ASC AUTOINCREMENT UNIQUE NOT NULL,
-          ebook_id INTEGER NOT NULL,
-          cat_id INTEGER NOT NULL,
-          FOREIGN KEY(ebook_id) REFERENCES ebooks(id),
-          FOREIGN KEY(cat_id) REFERENCES categories(id)
-        );
-        """)
-
-    conn.commit()
+    some_file = open("items.txt", "w+")
     for i in range(1, limit):
         for path in get_links(i):
             book = get_book(path)
-            out = c.execute(
-                "INSERT INTO ebooks "
-                "(title, img, des, isbn_10, isbn_13, format, author, pub_date, size, pub, pages, host) "
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", [
-                    book[0],
-                    book[1],
-                    book[3],
-                    book[2].get('isbn-10'),
-                    book[2].get('isbn-13'),
-                    book[2].get('format'),
-                    book[2].get('authors'),
-                    book[2].get('publication date'),
-                    book[2].get('size'),
-                    book[2].get('publisher'),
-                    book[2].get('pages'),
-                    book[4]
-                ])
-            conn.commit()
-            print(f'Inserted ({book[0]}, {book[1]}, ...) -> ebooks')
-
-            ebook_id = out.lastrowid
-            for cat in book[5]:
-                c.execute("""INSERT OR IGNORE INTO categories (name) VALUES (?)""", [cat])
-                c.execute("""SELECT id FROM categories WHERE name = ?""", [cat])
-                cat_id = c.fetchone()[0]
-                c.execute("""INSERT INTO categories_data (ebook_id, cat_id) VALUES (?, ?)""", [ebook_id, cat_id])
-                print(f'Inserted [{book[0]} {cat}] ({ebook_id}, {cat_id}) -> categories_data')
-
-            conn.commit()
-
-    c.close()
-    conn.close()
+            some_file.write(str(book) + '\n')
+    some_file.flush()
+    some_file.close()
 
 
 main()
